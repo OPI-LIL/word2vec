4348da1c12942e97e3e9c39074afe1b1c429298d
1o0ko <wojciech.stokowiec@gmail.com>
2015-10-15 16:24:39 +0200

    minor fixes

--- common_crawl/train.py
+++ common_crawl/train.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import sys
 import getopt
 import gensim
@@ -14,37 +15,10 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-def trainModel(args):
-    limit = args['limit']
-    workers = args['workers']
-    min_count = args['min_count']
-    size = args['size']
-    hostIp = args['hostIp']
-
-    logger.info('Training model with limit ' + str(limit) + ' workers ' + str(workers) + ' and minimum count '
-                 + str(min_count) + ' with vector size: ' + str(size))
-
-    # import data from cassandra
-    sentences = CassandraSentences(hostIp, 'ngramspace', limit)
-
-    try:
-        # train model
-        model = gensim.models.Word2Vec(sentences, min_count=min_count, workers=workers, size=size)
-
-        # Save model
-        logger.info('Saving model to file')
-
-        model.init_sims(replace=True)
-        model.save('/home/1o0ko/models/word2vec' + str(limit))
-
-        logger.info('Model has been saved.')
-
-    except Exception as exc:
-        logger.exception('Exception in model training: ' + str(exc))
-
 
 def parse_args(argv):
-    dict = {'limit': None, 'min_count': 5, 'workers': 1, 'size': 100, 'hostIp': '10.20.20.213'}
+    dict = {'limit': None, 'min_count': 5, 'workers': multiprocessing.cpu_count(), 'size': 100,
+            'hostIp': '10.20.20.213'}
 
     try:
         opts, args = getopt.getopt(argv, "hl:w:m:s:i:", ["limit=", "workers=", "min_count=", "size=", "hostIp="])
@@ -71,7 +45,29 @@ def parse_args(argv):
 
 
 if __name__ == "__main__":
+    # get arguments
     args = parse_args(sys.argv[1:])
-    trainModel(args)
+    limit, workers, min_count, size, hostIp = args['limit'], args['workers'], args['min_count'], args['size'], \
+                                              args['hostIp']
+
+    # log arguments
+    logger.info('Training with: ' + ' '.join([k + " : " + str(v) for k, v in args.iteritems()]))
+
+    # import data from cassandra
+    sentences = CassandraSentences(hostIp, 'ngramspace', limit)
+
+    try:
+        # train model
+        model = gensim.models.Word2Vec(sentences, min_count=min_count, workers=workers, size=size)
+
+        # Save model
+        logger.info('Saving model to file')
+        model.init_sims(replace=True)
+        model.save('/home/1o0ko/models/word2vec' + str(limit))
+
+        logger.info('Model has been saved.')
+
+    except Exception as exc:
+        logger.exception('Exception in model training: ' + str(exc))
 
     logger.info('Done!')
